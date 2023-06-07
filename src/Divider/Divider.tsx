b05533337d25cc1adce45ee3c6d46e97ff1a5b8c
Mosqueiro <erickcosta2010@gmail.com>
2023-06-06 22:21:44 -0300

    Update Divider.tsx

--- src/Divider/Divider.tsx
+++ src/Divider/Divider.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import Animated, { EasingNode } from 'react-native-reanimated';
+import Animated, { EasingNode as Easing } from 'react-native-reanimated';
 import { timing } from 'react-native-redash';
 
 import styles from './DividerStyles';
